301893f6c4866f0e038f65f2615b8cfb69b54cbb
Oliver Harrison <oliverdavidharrison@gmail.com>
2025-07-29 18:07:30 +0100

    slick

---
+++ assets/js/slick.js
@@ -0,0 +1,11 @@
+// assets/js/main.js
+$(document).ready(function(){
+    $('.slick-banner').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
