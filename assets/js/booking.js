26cf88d2e51711535235c5254070970a20cc42da
Oliver Harrison <oliverdavidharrison@gmail.com>
2026-02-05 10:30:23 +0000

    b

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,18 +1,8 @@
-function initBookingButton() {
-  const bookingBtn = document.getElementById('booking-button');
-
-  if (!bookingBtn) return console.error('Booking button not found');
-  if (!window.calendar || !calendar.schedulingButton) {
-    // Retry after a short delay if script not yet loaded
-    setTimeout(initBookingButton, 100);
-    return;
-  }
-
+window.addEventListener('load', function () {
   calendar.schedulingButton.load({
     url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3mQ014q6cvBlF69ElwyuoFeuNUZNMQmDJe37G2hDa32UJ3AvmkKPSFTR9YD-T400RmAAz9jhgf?gv=true',
-    target: bookingBtn
+    color: '#039BE5',
+    label: 'Check Availability',
+    target: document.getElementById('booking-button'),
   });
-}
-
-// Run on load
-window.addEventListener('load', initBookingButton);
+});
