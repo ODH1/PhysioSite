747b7727fd4d2dfa66cdfb2842aa2cf585053993
Oliver Harrison <oliverdavidharrison@gmail.com>
2026-02-05 10:12:11 +0000

    btn

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function () {
   calendar.schedulingButton.load({
     url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3mQ014q6cvBlF69ElwyuoFeuNUZNMQmDJe37G2hDa32UJ3AvmkKPSFTR9YD-T400RmAAz9jhgf?gv=true',
-    color: '#039BE5',
-    label: 'Book an appointment',
+    class: 'button',
+    label: 'Check Availability',
     target: document.getElementById('booking-button'),
   });
 });
