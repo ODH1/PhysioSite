8cdc21a0faa638281aeae50a809162c98a05cfd5
Oliver Harrison <oliverdavidharrison@gmail.com>
2026-02-06 11:02:33 +0000

    button fix

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function () {
 
   calendar.schedulingButton.load({
     url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3mQ014q6cvBlF69ElwyuoFeuNUZNMQmDJe37G2hDa32UJ3AvmkKPSFTR9YD-T400RmAAz9jhgf?gv=true',
-    color: '#039BE5',
+    color: '#474747',
     label: 'Check Availability',
     target: btn,
   });
